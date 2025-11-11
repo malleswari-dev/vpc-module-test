cb77df4cdc1db2a6fec74c5a24cd935428f366a1
malleswari <maddikirimalleswari4@gmail.com>
2025-11-11 23:09:42 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "vpc" {
+  # source = "../terraform-aws-vpc"
   source = "git::https://github.com/malleswari-dev/terraform-aws-vpc.git?ref=main"
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
