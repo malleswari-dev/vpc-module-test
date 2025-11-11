34935af03689be31fe02614bf2bedbc6e6b83b52
malleswari <maddikirimalleswari4@gmail.com>
2025-11-11 21:12:16 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "../terraform-aws-vpc"
+  source = "git::https://github.com/malleswari-dev/terraform-aws-vpc.git?ref=main"
   vpc_cidr = var.vpc_cidr
   project_name = var.project_name
   environment = var.environment
@@ -7,6 +7,7 @@ module "vpc" {
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required = true
   
   
 }
